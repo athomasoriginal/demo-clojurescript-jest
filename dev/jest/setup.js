6af55287680e93c31b36d8ec0018e6491377c5c6
tkjone <thomasmattacchione@gmail.com>
2018-08-18 16:54:01 -0400

    init a WORKING jest setupFile
    This is what a working jest setup file looks like...at least for the
    simple example I have here

--- dev/jest/setup.js
+++ dev/jest/setup.js
@@ -1,3 +1,32 @@
-// Jest setup file
-//
-// require('./bootstrap_node');
+var fs = require('fs');
+var vm = require('vm');
+var path = require('path');
+
+global.CLOSURE_BASE_PATH = '../../target/public/cljs-out/test/goog/';
+
+global.CLOSURE_NO_DEPS = true;
+
+global.CLOSURE_IMPORT_SCRIPT = function(src) {
+  require(src);
+  return true;
+};
+
+var globalContext = vm.createContext(global);
+
+const content = fs.readFileSync(
+  path.resolve(__dirname, '../../target/public/cljs-out/test/goog/base.js')
+);
+
+const contentThree = fs.readFileSync(
+  path.resolve(__dirname, '../../target/public/cljs-out/test/goog/deps.js')
+);
+
+const contentTwo = fs.readFileSync(
+  path.resolve(__dirname, '../../target/public/cljs-out/test/cljs_deps.js')
+);
+
+vm.runInContext(content, globalContext, { filename: 'base.js' });
+
+vm.runInContext(contentThree, globalContext, { filename: 'deps.js' });
+
+vm.runInContext(contentTwo, globalContext, { filename: 'cljs_deps.js' });
